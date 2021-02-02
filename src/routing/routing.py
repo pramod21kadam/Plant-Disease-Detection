a8c6f12471249be23daa055deb190719b9698a15
pramod kadam <pramod21dkadam@gmail.com>
2021-02-02 13:12:08 +0530

    wrapper example in route

--- src/routing/routing.py
+++ src/routing/routing.py
@@ -1,7 +1,21 @@
-from packages.packages import redirect, render_template, session
+from packages.packages import (redirect, render_template, url_for)
 from application import App
+from utils.auth import token_required
 app = App()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Home"
+
+@app.route('/user')
+@token_required
+def user(current_user):
+    """
+        `current_user` content is passed by @token_required which contains the jwt token information of current user
+        Only verified users can access paths which are wrapped by @token_required
+    """
+    return "Only protected content"
+
+@app.route('/login', methods = ['GET'])
+def login():
+    return render_template('login.html')
