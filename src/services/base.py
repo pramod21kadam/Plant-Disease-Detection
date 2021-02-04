12e002c6d8e269f066c72190367cb8855cb3c842
pramod kadam <pramod21dkadam@gmail.com>
2021-02-04 13:06:11 +0530

    removed errors

--- src/services/base.py
+++ src/services/base.py
@@ -6,5 +6,6 @@ from utils.utils import classes
 def generate_image(filename,img_width = 224, img_height=224):
     test_image = image.load_img(filename, target_size=(img_width, img_height))
     test_image = image.img_to_array(test_image)
+    test_image = test_image/255.
     test_image = np.expand_dims(test_image, axis=0)
     return test_image
