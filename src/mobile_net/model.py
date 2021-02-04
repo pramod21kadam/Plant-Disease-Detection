7d9791b6db033e0f9dd8b719f35e32c74a3a06ef
pramod kadam <pramod21dkadam@gmail.com>
2021-02-04 13:05:45 +0530

    code correction

--- src/mobile_net/model.py
+++ src/mobile_net/model.py
@@ -7,7 +7,7 @@ class Metaclass(type):
             cls.__instance = super(Metaclass, cls).__call__(*args, **kwargs)   
         return cls.__instance
 
-class Model:
+class Model(metaclass = Metaclass):
     def __new__(self):
         self.instance = load_model('assets/mobile_net_9756999611854553.h5')
         return self.instance
