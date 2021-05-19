f22384e04190043aae715b056f37eae94d0a3672
OnkarKunjir <onkarkunjir8@gmail.com>
2021-05-19 14:45:18 +0530

    minor changes

--- src/mobile_net/model.py
+++ src/mobile_net/model.py
@@ -1,13 +1,16 @@
 from .base import *
 
+
 class Metaclass(type):
     __instance = None
+
     def __call__(cls, *args, **kwargs):
         if cls.__instance is None:
-            cls.__instance = super(Metaclass, cls).__call__(*args, **kwargs)   
+            cls.__instance = super(Metaclass, cls).__call__(*args, **kwargs)
         return cls.__instance
 
-class Model(metaclass = Metaclass):
-    def __new__(self):
-        self.instance = load_model('assets/mobile_net_9756999611854553.h5')
-        return self.instance
+
+class Model(metaclass=Metaclass):
+    def __new__(cls):
+        self.instance = load_model("assets/mobile_net_9756999611854553.h5")
+        return cls.instance
--- src/services/authentationServ.py
+++ src/services/authentationServ.py
@@ -1,17 +1,16 @@
 import re
 from .base import *
+
+
 class AuthentationServ:
     def sign_in(self, email, password):
         # pass data to data access object to check for valid user
-        obj = LoginDao. sign_in(email=email, password=password)
-        if len(obj)==1:
-            data = {
-                'email': email,
-                'id': obj[0].id
-            }
+        obj = LoginDao.sign_in(email=email, password=password)
+        if len(obj) == 1:
+            data = {"email": email, "id": obj[0].id}
             # Generate and return jwt token
             return True, generate_token(data=data)
-        return False, 'Failed Sign in'
+        return False, "Failed Sign in"
 
     def sign_up(self, email, password):
         try:
@@ -19,7 +18,7 @@ class AuthentationServ:
             boolean, msg = self.sign_in(email=email, password=password)
             # if yes return 'user already present in database'
             if boolean:
-                return False, 'user already present in database'
+                return False, "user already present in database"
             # if not send the data to data access layer to insert it into database
             return LoginDao.sign_up(email=email, password=password)
         except Exception as error:
