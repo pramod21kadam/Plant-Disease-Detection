37c624a720fc39c840d0a150c09dc21dc5af4bef
pramod kadam <pramod21dkadam@gmail.com>
2021-05-21 17:03:59 +0530

    Use @token_required wrapper wherever authorization needed

--- src/controller/predictionCtrl.py
+++ src/controller/predictionCtrl.py
@@ -1,26 +1,10 @@
 from .base import *
-import requests
-
-api_url = "http://127.0.0.1:5000/api/validate-token"
-
 
 class PredictionCtrl(MethodView):
-    # @token_required
+    @token_required
     def post(self):
         try:
             # get file
-            token = request.cookies.get("x-access-token")
-            if token:
-                data = {"x-access-token": token}
-                response = requests.post(api_url, data)
-                if (
-                    response.status_code != 200
-                    or response.json()["status"] != "success"
-                ):
-                    return response.json(), 400
-            else:
-                return failureRes(msg="invalid access"), 400
-
             file = request.files["predict_img"]
             if file:
                 boolean, result = PredictServ().predict(imageFile=file)
